689da83b7bbcc5dc40eb61228c54a72dcbd6b861
Samantha Helene Thomas <2shthomas@gmail.com>
2019-04-04 11:18:11 -0600

    first commit

---
+++ JS.js
@@ -0,0 +1,22 @@
+
+
+<html>
+    <head>
+        <script type="text/javascript">
+
+            function fun1(){
+
+                document.write("this is a function");
+
+
+            }
+
+            fun1();
+
+        </script>
+    <body>
+
+    </body>
+
+    </head>
+</html>
